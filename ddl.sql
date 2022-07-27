f9b4087f98087cad7a84ebe41d4033ef152684b9
Diana Dou <douxinyi0142@gmail.com>
2022-07-27 16:52:35 -0700

    Minor fix1

--- ddl.sql
+++ ddl.sql
@@ -32,7 +32,7 @@ CREATE TABLE Sponsorship(
 
 CREATE TABLE Sponsor(
     constructor_name CHAR[15],
-    sponsorship_name CHAR[15]
+    sponsorship_name CHAR[50],
     PRIMARY KEY(constructor_name,sponsorship_name),
     FOREIGN KEY(constructor_name) REFERENCES Constructors,
     FOREIGN KEY(sponsorship_name) REFERENCES Sponsorship
