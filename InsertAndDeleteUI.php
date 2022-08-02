1fff47b7ffbb33fa73a5c2c7c154bcdf854fdbce
UBC Student <qdkaiyu@students.cs.ubc.ca>
2022-08-01 18:54:02 -0700

    updated the ui

--- InsertAndDeleteUI.php
+++ InsertAndDeleteUI.php
@@ -1,35 +1,33 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>
-            <meta charset = "UTF-8">
-            Insert circuits
-        </title>
-    </head>
 
-    <body>
+<head>
+  <title>
+    Insert circuits
+  </title>
+  <link rel="stylesheet" href="styles.css">
+</head>
 
-    <h2 style="color:MediumSeaGreen;">Adding new Circuits</h2>
-    <form method="POST" action="InsertAndDeleteUI.php"> 
-        <input type="hidden" id="insertQueryRequest" name="insertQueryRequest">
-        Circuit Name: <input type="text" name="circuit_name"> <br /><br />
-        <p>Leave below entries empty if you do not have detailed information.</p>
-        City: <input type="text" name="city"> <br /><br />
-        Country: <input type="text" name="country"> <br /><br />
-        Longitude: <input type="real" name="longitude"> <br /><br />
-        Latitude: <input type="real" name="latitude"> <br /><br />
-
-        <input type="submit" value="Insert" name="insertSubmit"></p>
-
-    </body>
-    </form>
-
-    <h2 style="color:Tomato;">Delete Circuit</h2>
-    <form method="POST" action="InsertAndDeleteUI.php">
-        <input type="hidden" id="deleteQueryRequest" name="deleteQueryRequest">
-        Delete Circuit Name: <input type="text" name="circuit_name"> <br /><br />
-        <input type="submit" value="Delete" name="deleteSubmit"></p>
-
-        
+<body>
+  <header class="page-header">
+    <h1>Formula Database Application </h1>
+  </header>
+  <h2 class="operations">Adding new Circuits</h2>
+  <form method="POST" action="InsertAndDeleteUI.php">
+    <input type="hidden" id="insertQueryRequest" name="insertQueryRequest">
+    Circuit Name: <input type="text" name="circuit_name"> <br /><br />
+    <p>Leave below entries empty if you do not have detailed information.</p>
+    City: <input type="text" name="city"> <br /><br />
+    Country: <input type="text" name="country"> <br /><br />
+    Longitude: <input type="real" name="longitude"> <br /><br />
+    Latitude: <input type="real" name="latitude"> <br /><br />
+    <input type="submit" value="Insert" name="insertSubmit"></p>
+</body>
+</form>
+<h2 class="operations">Delete Circuit</h2>
+<form method="POST" action="InsertAndDeleteUI.php">
+  <input type="hidden" id="deleteQueryRequest" name="deleteQueryRequest">
+  Circuit Name: <input type="text" name="circuit_name"> <br /><br />
+  <input type="submit" value="Delete" name="deleteSubmit"></p>
 
 </html>
