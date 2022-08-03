d588bd4b16a831729621b850ec8c993cb9d7f390
Diana Dou <douxinyi0142@gmail.com>
2022-08-03 16:20:43 -0700

    Minor fix

--- MainUI.php
+++ MainUI.php
@@ -4,9 +4,22 @@
             <meta charset = "UTF-8">
             CPSC 304 Formula 1 Database Application
         </title>
+        <link rel="stylesheet" href="styles.css">
     </head>
     <body>
-    
+    <header class="page-header">
+    <h1>Formula 1 Database Application </h1>
+    </header>
+    <br />
+
+  <h2 class="operations">
+    Welcome to Formula 1 database application! Please select below options:</h2>
+    <br />
+
+<form method="GET" action="https://www.students.cs.ubc.ca/~douxinyi/m4/InsertAndDeleteUI.php">
+    <p>Checking Circuits:&nbsp;
+        <input type="submit" value="Circuit" name="Circuit"></p>
+</form>
     </body>
 
 </html>
