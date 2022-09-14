f24f6fee14ee4fc9fd64e78ae5033ff043158d33
Reby0217 <100398733+Reby0217@users.noreply.github.com>
2022-09-13 17:46:01 -0700

    connect to own UBC Account Oracle

--- Formula 1 Database Application/Code/#OracleFunctions.php
+++ Formula 1 Database Application/Code/#OracleFunctions.php
@@ -70,7 +70,7 @@ function connectToDB()
   {
     global $db_conn;
     //change to your cwl and password
-    $db_conn = OCILogon("ora_douxinyi", "a84855964", "dbhost.students.cs.ubc.ca:1522/stu");
+    $db_conn = OCILogon("ora_kej19", "a16752370", "dbhost.students.cs.ubc.ca:1522/stu");
     if ($db_conn) {
       debugAlertMessage("Database is Connected");
       return true;
